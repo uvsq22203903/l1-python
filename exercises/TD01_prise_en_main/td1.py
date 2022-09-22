de7ae355cdef163a06bd443f9418f595090153d7
Emma CLUZET <emma.cluzet@ens.uvsq.fr>
2022-09-22 12:43:53 +0200

    update

--- exercises/TD01_prise_en_main/td1.py
+++ exercises/TD01_prise_en_main/td1.py
@@ -1,4 +1,4 @@
-a = 1 + 1
+"""a = 1 + 1
 print(a)
 a += 1
 print(a)
@@ -8,3 +8,44 @@ print(a,b)
 a = b
 b = 5
 print(a,b)
+
+type(5)
+type(5.0)
+type("5")
+type(-5)
+type(5/2)
+type(5//2)
+type(5==2)
+type(5=2)
+
+x = 47
+print("le type de x est", type(x))
+
+var1=3 * (10 / 3)
+var2=3 * (10 // 3)
+var3=(3 * 10) // 3
+
+print(var1,var2,var3)"""
+
+"""3.3 chaîne de caractère"""
+"""
+a = "hello"
+b = 'world'
+print(a+" "+b)
+print(b+" "+a)
+
+r= a*3
+h=(a +b) * 2
+print(r,h)
+
+print(a.upper())
+
+print(a > b)"""
+a = "32"
+a=int("32")
+print(a*2)
+
+nombre = int(input(""))
+print(nombre*2)
+
+age = int(input("Vous aurait ",age,"en 2031 le même jour ?"))
