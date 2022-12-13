1d9b46f01ee0219c51958c51494bf2f8461ad783
Emma CLUZET <emma.cluzet@ens.uvsq.fr>
2022-12-13 15:06:47 +0100

    nouveau

---
+++ CC1.py
@@ -0,0 +1,10 @@
+
+
+
+# Question 14
+a = [2, 5, 1]
+x = 3
+b, y = a, x
+a[0] = 0
+x = 1
+print(b[0], y)
---
+++ CC2_entrainement.py
@@ -0,0 +1,21 @@
+"""
+def f(n):
+    cpt = 0
+    while n != 1:
+        cpt += 1
+        n = n // 2
+    return cpt
+
+
+print(f(18))
+
+
+a = [3, 4] + [[1, 2]] * 2
+print(a[-1][-1])
+"""
+
+for i in range(5):
+    print("x"*(5-i))
+
+a = [3, 5, 2, 7]
+print(a[-1])
