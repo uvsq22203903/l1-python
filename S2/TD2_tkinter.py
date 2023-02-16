39c00ff15136cf39047f02b7bd2468edcc2b2ddf
Emma CLUZET <emma.cluzet@ens.uvsq.fr>
2023-02-16 21:44:25 +0100

    update

--- S2/TD2_tkinter.py
+++ S2/TD2_tkinter.py
@@ -1,24 +1,39 @@
+# -*-coding:Utf-8 -*
 import tkinter as tk
+
+# - - - - - - - - - - - - -
+# Déclaration des fonctions
+# - - - - - - - - - - - - -
+
+def fermer_fenetre():  # fonction callback qui appelle la fonction destroy()
+    root.destroy()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - 
 # Création de la fenêtre de l'interface graphique
+# - - - - - - - - - - - - - - - - - - - - - - - -
+
 root = tk.Tk()
 root.title("J'apprends Tkinter")
+root.geometry("400x300")
+caneva = Canvas(root, width = 500, height = 500, bg ='red')
+caneva.place(x = 100, y = 100)
 
- # fonction callback qui appelle la fonction destroy()
-def fermer_fenetre():
-    pass
-
+# - - - - - - - - - - - - -
+# Création d'un label texte
+# - - - - - - - - - - - - -
 
 texte = tk.Label (root, text = "J'adore Python")
-bouton = tk.Button(root, text="Quitter", command=root.destroy)
-
-
-
-
-
-
-
 texte.pack()
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Création d'un bouton lancant la fonction fermer_fenetre
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+bouton = tk.Button(root, text="Quitter", command = lambda: fermer_fenetre())
 bouton.pack()
+
+
 root.mainloop # permet de toujours actualiser la fenêtre
- # programme principal
+
+# programme principal
 print("finish")
