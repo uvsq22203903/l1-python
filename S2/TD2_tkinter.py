e3df52935770aa78b8f6afbff45c1415fa94d509
Emma CLUZET <emma.cluzet@ens.uvsq.fr>
2023-02-16 20:41:07 +0100

    update

--- S2/TD2_tkinter.py
+++ S2/TD2_tkinter.py
@@ -1,12 +1,24 @@
 import tkinter as tk
-
+# Création de la fenêtre de l'interface graphique
 root = tk.Tk()
 root.title("J'apprends Tkinter")
+
+ # fonction callback qui appelle la fonction destroy()
+def fermer_fenetre():
+    pass
+
+
 texte = tk.Label (root, text = "J'adore Python")
-bouton = tk.Button(root, text="Quitter", command=root.quit)
+bouton = tk.Button(root, text="Quitter", command=root.destroy)
+
+
+
+
+
+
 
 texte.pack()
 bouton.pack()
-root.mainloop
-
-
+root.mainloop # permet de toujours actualiser la fenêtre
+ # programme principal
+print("finish")
