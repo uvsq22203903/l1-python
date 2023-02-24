8c12b9597afe96143d66a702554ced7abc1b89f1
Emma CLUZET <emma.cluzet@ens.uvsq.fr>
2023-02-24 17:01:55 +0100

    update

--- examples/gui/example03.py
+++ examples/gui/example03.py
@@ -1,22 +1,23 @@
-from tkinter import *
+import tkinter as tk
 
-CANVAS_WIDTH, CANVAS_HEIGHT = 600, 400
+CANVAS_WIDTH, CANVAS_HEIGHT = 600, 500
 
-if __name__ == '__main__':
-    root = Tk()
 
-    canvas = Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+root = tk.Tk()
 
-    # Début de votre code
-    x0 = 100
-    x1 = CANVAS_WIDTH - 100
-    y = CANVAS_HEIGHT / 2
-    canvas.create_line(x0, y, x1, y)
-    canvas.create_oval(x0 - 50, y + 50, x0 + 50, y - 50)
-    canvas.create_oval(x1 - 50, y + 50, x1 + 50, y - 50)
-    canvas.create_oval((x0 + x1) / 2 - 50, y + 50, (x0 + x1) / 2 + 50, y - 50)
+canvas = tk.Canvas(root, width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+
+# Début de votre code
+x0 = 300
+x1 = CANVAS_WIDTH - 300
+y1 = 100
+y2 = 400
+canvas.create_line(x0, y1, x1, y2)
+canvas.create_oval(250,50,350,150)
+canvas.create_oval(250, 200, 350, 300)
+canvas.create_oval(250, 350, 350, 450)
     
-    # Fin de votre code
+# Fin de votre code
 
-    canvas.pack()
-    root.mainloop()
+canvas.pack()
+root.mainloop()
